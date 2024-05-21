d38e1b1c404084725d75ec519b098bae196703dd
shamhi <shomakakdela@gmail.com>
2024-05-21 19:09:08 +0300

    🛠 Fix flood wait

--- bot/core/claimer.py
+++ bot/core/claimer.py
@@ -204,11 +204,16 @@ class Claimer:
 
 async def run_claimer(tg_client: Client, db_pool: async_sessionmaker):
     try:
-        async with tg_client:
-            user_data = await tg_client.get_me()
-
         if not local_db.get(tg_client.name):
-            local_db[tg_client.name] = {'Token': '', 'ClaimTime': 0}
+            local_db[tg_client.name] = {'UserData': None, 'Token': '', 'ClaimTime': 0}
+
+        if not local_db[tg_client.name]['UserData']:
+            async with tg_client:
+                user_data = await tg_client.get_me()
+
+                local_db[tg_client.name]['UserData'] = user_data
+        else:
+            user_data = local_db[tg_client.name]['UserData']
 
         proxy = None
         if settings.USE_PROXY_FROM_DB:
